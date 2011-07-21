1d14bd99c4a1682f59c035a74d01f3a41ded4670
mfairchild365 <mfairchild365@gmail.com>
2011-07-21 14:20:59 -0500

    add a route for successes

--- data/Routes.php
+++ data/Routes.php
@@ -13,4 +13,7 @@ $routes['/^logout$/i'] = 'Wub_Account_Logout';
 //Logout
 $routes['/^register$/i'] = 'Wub_Account_Register';
 
+//Logout
+$routes['/^success$/i'] = 'Wub_Success';
+
 return $routes;
