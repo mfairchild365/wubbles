82b9e660ff59c43d3ef90d30bce90fefd189e1d9
mfairchild365 <mfairchild365@gmail.com>
2011-07-21 14:28:41 -0500

    style the register form and make it ajax.

--- www/templates/default/Wub/Account/Register.tpl.php
+++ www/templates/default/Wub/Account/Register.tpl.php
@@ -1,23 +1,13 @@
-Register.
-<form name="input" action="<?php echo Wub_Controller::$url; ?>register" method="post">
-    <ul>
-        <li>
-            email: <input type="text" name="email" />
-        </li>
-        <li>
-            retype email: <input type="text" name="email2" />
-        </li>
-        <li>
-            username  <input type="text" name="username" />
-        </li>
-        <li>
-            password  <input type="password" name="password" />
-        </li>
-        <li>
-            retype pssword  <input type="password" name="password2" />
-        </li>
-    </ul>
+<form class='ajaxform' name="input" action="<?php echo Wub_Controller::$url; ?>register" method="post">
+    <fieldset>
+    <legend>Register</legend>
+        <p>email: <br /><input type="text" name="email" /></p>
+        <p>retype email: <br /><input type="text" name="email2" /></p>
+        <p>username: <br /><input type="text" name="username" /></p>
+        <p>password: <br /><input type="password" name="password" /></p>
+        <p>retype pssword: <br /><input type="password" name="password2" /></p>
     <input type="hidden" name="_type" value='register'/>
     <input type="hidden" name="_class" value='<?php echo get_class($context->getRawObject()); ?>'/>
     <input type="submit" value="Submit" />
+    </fieldset>
 </form> 
