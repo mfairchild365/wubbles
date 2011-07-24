98b9a017664935ba2fba6bd7076d287fe3059b37
mfairchild365 <mfairchild365@gmail.com>
2011-07-24 02:01:16 -0500

    Fix permissions.

--- src/Wub/Memory/View.php
+++ src/Wub/Memory/View.php
@@ -5,8 +5,16 @@ class Wub_Memory_View extends Wub_Memory
     {
         parent::__construct($options);
         
-        if ($this->permission == 'public') {
-            return;
+        switch ($this->permission) {
+            case 'public':
+                break;
+            case 'friends':
+                throw new exception('This function does not work yet.');
+            case 'private':
+            default:
+                if (!Wub_Controller::getAccount() || Wub_Controller::getAccount()->id != $this->owner_id) {
+                    throw new Exception("You do not have permission to view this.");
+                }
         }
     }
 }
