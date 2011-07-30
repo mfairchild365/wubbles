db2c72fcbaf6f72cc130f5387d1801b3cd689f68
mfairchild365 <mfairchild365@gmail.com>
2011-07-29 22:29:52 -0500

    Remove the ajax login...  It was a nice idea, but wasn't working out so well.

--- www/templates/default/Wub/UserInfo/Loggedout.tpl.php
+++ www/templates/default/Wub/UserInfo/Loggedout.tpl.php
@@ -1,24 +1,3 @@
-<script type="text/javascript">
-$(function(){
-    $('#logindiv').hide();
-    $('#login').click(function() {
-        if ($('#logindiv').is(":visible")) {
-            $('#logindiv').hide();
-            return false;
-        }
-        $.ajax({
-            url: "<?php echo Wub_Controller::$url . "login?format=partial"?>",
-            success: function(data){
-            $('#logindiv').html(data);
-            initAjaxForms();
-            $('#logindiv').show();
-            }
-        });
-        return false;
-    });
-});
-</script>
-
-<a href="<?php echo Wub_Controller::$url . "login"?>" class="ui-state-default ui-corner-all" id='login' title="login" style='float:right'><span class="ui-icon ui-icon-power"></span></a>
-<div id='logindiv'>
-</div>
+<a href="<?php echo Wub_Controller::$url . "register"?>" title="register">Register</a> 
+or 
+<a href="<?php echo Wub_Controller::$url . "login"?>" title="login">Login</a>
