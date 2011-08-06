2b80530730b273e69292d8e75dcd13d52b01fe44
mfairchild365 <mfairchild365@gmail.com>
2011-08-06 13:50:35 -0500

    another attempt.

--- src/Wub/Utilities.php
+++ src/Wub/Utilities.php
@@ -4,8 +4,23 @@ class Wub_Utilities
     static function formatTime($timeStamp)
     {
         if ($account = Wub_Controller::getAccount()) {
-            $time = new DateTime(date("F j, Y, g:i a", $timeStamp), new DateTimeZone($account->timezone));
-            $timeStamp = $time->format('U');
+            // set this to the time zone provided by the user
+            $tz = $account->timezone;
+             
+            // create the DateTimeZone object for later
+            $dtzone = new DateTimeZone($tz);
+             
+            // create a DateTime object
+            $dtime = new DateTime();
+             
+            // set it to the timestamp (PHP >= 5.3.0)
+            $dtime->setTimestamp($timeStamp);
+             
+            // convert this to the user's timezone using the DateTimeZone object
+            $dtime->setTimeZone($dtzone);
+             
+            // print the time using your preferred format
+            return $dtime->format('g:i A m/d/y');
         }
         
         return  date("F j, Y, g:i a", $timeStamp);
